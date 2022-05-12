ffc22ffc812740bbd4fcc5ceb8760f43399000fb
Kerby Tabares <kerby.tabares@tutenlabs.com>
2022-05-12 10:07:11 -0400

    ultimo

--- app.js
+++ app.js
@@ -135,14 +135,14 @@ angular.module('app', [
           }else if(response.status == -1){
             
             // ngNotify.set('¡No existe conexion con el servidor por favor intente mas tarde!'  ,'error')
-            $('body').append('<div id="notify" class="ngn ng-scope ngn-error ngn-bottom animated fadeOut" style="display: block; opacity: 1;"><span  class="ngn-message ng-binding ng-scope">¡No existe conexion con el servidor por favor intente mas tarde!</span><span  class="ngn-dismiss ng-hide">×</span></div>')
+            $('body').append('<div id="notify" class="ngn ng-scope ngn-error ngn-bottom animated fadeOut" style="display: block; opacity: 1;"><span  class="ngn-message ng-binding ng-scope">¡No existe conexion con el servidor, por favor intente mas tarde!</span><span  class="ngn-dismiss ng-hide">×</span></div>')
             
             $location.path('/');
             setTimeout(function () {
               $('#notify').remove()
             },5000)
           }else if(response.status==500){
-            $('body').append('<div id="notify" class="ngn ng-scope ngn-error ngn-bottom animated fadeOut" style="display: block; opacity: 1;"><span  class="ngn-message ng-binding ng-scope">¡Estamos presentando problemas con el servidor por favor intente mas tarde!</span><span  class="ngn-dismiss ng-hide">×</span></div>')
+            $('body').append('<div id="notify" class="ngn ng-scope ngn-error ngn-bottom animated fadeOut" style="display: block; opacity: 1;"><span  class="ngn-message ng-binding ng-scope">¡Estamos presentando problemas con el servidor, por favor intente mas tarde!</span><span  class="ngn-dismiss ng-hide">×</span></div>')
             
             $location.path('/');
             setTimeout(function () {
